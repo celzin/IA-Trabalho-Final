0f79ccb6616373a0f377e08f80bf803b33e45ce4
celsin <celsovinicius4@gmail.com>
2024-09-07 15:46:11 -0300

    descomentando funções

--- src/main.py
+++ src/main.py
@@ -5,21 +5,21 @@ from visualizacao import visualizar_grafo
 def main():
     grafo = criar_grafo_romenia()
 
-    # print("\nA* com heurística Euclidiana:")
-    # (caminho, distancia, nos_explorados, log), tempo_execucao = medir_tempo_execucao(a_estrela, grafo.nos, 'Arad', 'Bucharest', heuristica_euclidiana, coordenadas)
-    # print(f"Caminho: {caminho}, Distância: {distancia}, Nós explorados: {nos_explorados}, Tempo de execução: {tempo_execucao:.4f} ms")
-    # print("Sequência de nós explorados:")
-    # for entry in log:
-    #     print(entry)
-    # visualizar_grafo(grafo.nos, caminho)
+    print("\nA* com heurística Euclidiana:")
+    (caminho, distancia, nos_explorados, log), tempo_execucao = medir_tempo_execucao(a_estrela, grafo.nos, 'Arad', 'Bucharest', heuristica_euclidiana, coordenadas)
+    print(f"Caminho: {caminho}, Distância: {distancia}, Nós explorados: {nos_explorados}, Tempo de execução: {tempo_execucao:.4f} ms")
+    print("Sequência de nós explorados:")
+    for entry in log:
+        print(entry)
+    visualizar_grafo(grafo.nos, caminho)
 
-    # print("\nA* com heurística Manhattan:")
-    # (caminho, distancia, nos_explorados, log), tempo_execucao = medir_tempo_execucao(a_estrela, grafo.nos, 'Arad', 'Bucharest', heuristica_manhattan, coordenadas)
-    # print(f"Caminho: {caminho}, Distância: {distancia}, Nós explorados: {nos_explorados}, Tempo de execução: {tempo_execucao:.4f} ms")
-    # print("Sequência de nós explorados:")
-    # for entry in log:
-    #     print(entry)
-    # visualizar_grafo(grafo.nos, caminho)
+    print("\nA* com heurística Manhattan:")
+    (caminho, distancia, nos_explorados, log), tempo_execucao = medir_tempo_execucao(a_estrela, grafo.nos, 'Arad', 'Bucharest', heuristica_manhattan, coordenadas)
+    print(f"Caminho: {caminho}, Distância: {distancia}, Nós explorados: {nos_explorados}, Tempo de execução: {tempo_execucao:.4f} ms")
+    print("Sequência de nós explorados:")
+    for entry in log:
+        print(entry)
+    visualizar_grafo(grafo.nos, caminho)
 
     print("\nA* com heurística hDLR:")
     (caminho, distancia, nos_explorados, log), tempo_execucao = medir_tempo_execucao(a_estrela, grafo.nos, 'Arad', 'Bucharest', heuristica_hDLR)
@@ -27,7 +27,7 @@ def main():
     print("Sequência de nós explorados:")
     for entry in log:
         print(entry)
-    # visualizar_grafo(grafo.nos, caminho)
+    visualizar_grafo(grafo.nos, caminho)
 
     print("\nBusca em Largura (BFS):")
     (caminho, nos_explorados, log), tempo_execucao = medir_tempo_execucao(bfs, grafo.nos, 'Arad', 'Bucharest')
@@ -35,15 +35,15 @@ def main():
     print("Sequência de nós explorados:")
     for entry in log:
         print(entry)
-    # visualizar_grafo(grafo.nos, caminho)
+    visualizar_grafo(grafo.nos, caminho)
 
-    # print("\nBusca em Profundidade (DFS):")
-    # (caminho, nos_explorados, log), tempo_execucao = medir_tempo_execucao(dfs, grafo.nos, 'Arad', 'Bucharest')
-    # print(f"Caminho: {caminho}, Nós explorados: {nos_explorados}, Tempo de execução: {tempo_execucao:.4f} ms")
-    # print("Sequência de nós explorados:")
-    # for entry in log:
-    #     print(entry)
-    # visualizar_grafo(grafo.nos, caminho)
+    print("\nBusca em Profundidade (DFS):")
+    (caminho, nos_explorados, log), tempo_execucao = medir_tempo_execucao(dfs, grafo.nos, 'Arad', 'Bucharest')
+    print(f"Caminho: {caminho}, Nós explorados: {nos_explorados}, Tempo de execução: {tempo_execucao:.4f} ms")
+    print("Sequência de nós explorados:")
+    for entry in log:
+        print(entry)
+    visualizar_grafo(grafo.nos, caminho)
 
 if __name__ == "__main__":
     main()
